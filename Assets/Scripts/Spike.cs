d59829c8ee123c416ba6388e56a67eddff312849
mandtc <colin.mandt@gmail.com>
2022-05-22 12:39:07 -0700

    Added spikes

---
+++ Assets/Scripts/Spike.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    BoxCollider2D BoxColliderWithTrigger;
+    BoxCollider2D BoxColliderNoTrigger;
+
+    void Awake()
+    {
+        BoxColliderWithTrigger = gameObject.AddComponent<BoxCollider2D>() as BoxCollider2D;
+        BoxColliderWithTrigger.isTrigger = true;
+        BoxColliderNoTrigger = gameObject.AddComponent<BoxCollider2D>() as BoxCollider2D;
+        Vector2 ColliderSize;
+        ColliderSize = new Vector2(0.09f, 0.001f);
+        BoxColliderNoTrigger.size = ColliderSize;
+    }
+
+    void OnTriggerEnter2D(Collider2D Collider)
+    {
+        Collider.GetComponent<PlayerController>().TakeDamage(25);
+    }
+}
